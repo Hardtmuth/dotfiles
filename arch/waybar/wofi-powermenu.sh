a1ea32a320ee737d903463ff21626189e5e96f17
Andrey Abramov <abramow-a.v@ya.ru>
2024-05-05 18:23:20 +0300

    upd arch files

---
+++ arch/waybar/wofi-powermenu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+entries="⇠ Logout\n⏾ Suspend\n Lock\n⭮ Reboot\n⏻ Shutdown"
+
+selected=$(echo -e $entries|wofi --dmenu --lines 6 --cache-file /dev/null | awk '{print tolower($2)}')
+
+case $selected in
+  logout)
+    killhypr.sh;;
+  suspend)
+    exec systemctl suspend;;
+  reboot)
+    exec systemctl reboot;;
+  shutdown)
+    exec systemctl poweroff -i;;
+  lock)
+    exec swaylock;;
+esac
