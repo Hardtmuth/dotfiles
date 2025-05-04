a54e920cc3735972a7baa946a09ca081c71dd05b
Hardtmuth <abramow-a.v@ya.ru>
2025-05-04 14:05:47 +0300

    after reinstall

---
+++ arch/hypr/scripts/output_monitor.py
@@ -0,0 +1,29 @@
+#!/bin/python
+
+import subprocess
+
+
+output = subprocess.check_output("hyprctl monitors", shell=True, text=True)
+
+#if "ID 0" and "ID 1" and "ID 2" in output:
+#    subprocess.run(['sed', '-i', 's/"eDP-1"/"DP-2"/g', '/home/link/.config/waybar/config.jsonc'])
+#
+#elif "ID 0" and "ID 1" in output:
+#    subprocess.run(['sed', '-i', 's/"eDP-1"/"DP-2"/g', '/home/link/.config/waybar/config.jsonc'])
+#    subprocess.run(['sed', '-i', 's/"DP-1"/"DP-2"/g', '/home/link/.config/waybar/config.jsonc'])
+#else:
+#    subprocess.run(['sed', '-i', 's/"DP-1"/"eDP-1"/g', '/home/link/.config/waybar/config.jsonc'])
+#    subprocess.run(['sed', '-i', 's/"DP-2"/"eDP-1"/g', '/home/link/.config/waybar/config.jsonc'])
+
+# if "DELL" in output:
+#    subprocess.run(['sed', '-i', 's/"DP-1"/"DP-2"/g', '/home/link/.config/waybar/config.jsonc'])
+#    subprocess.run(['sed', '-i', 's/"eDP-1"/"DP-2"/g', '/home/link/.config/waybar/config.jsonc'])
+
+if "Iiyama" in output:
+    #subprocess.run(['sed', '-i', 's/"DP-1"/"HDMI-A-1"/g', '/home/link/.config/waybar/config.jsonc'])
+    subprocess.run(['sed', '-i', 's/"eDP-1"/"HDMI-A-1"/g', '/home/link/.config/waybar/config.jsonc'])
+
+
+else:
+    subprocess.run(['sed', '-i', 's/"HDMI-A-1"/"eDP-1"/g', '/home/link/.config/waybar/config.jsonc'])
+    #subprocess.run(['sed', '-i', 's/"HDMI-A-1"/"eDP-1"/g', '/home/link/.config/waybar/config.jsonc'])
---
+++ arch/waybar/scripts/shortcuts.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+msg1=`cat <<_EOF_
+Logout:               + M
+Lock:                 + ALT + L
+
+Terminal:             + RETURN
+Browser:              + B
+Files:                + E
+Launcher:             + SPACE
+
+Screenshots:
+window:               + SHIFT + S
+region:               + PRINT
+screen:              PRINT
+
+Clipboard history:    + V
+_EOF_`
+
+msg2=`cat <<_EOF_
+Close window:         + Q
+Fullscreen:           + F
+Float:                + T
+Rotate:               + R
+
+Move window:          + 󰳽  
+Resize window:        +  󰳽
+
+Move focus:           + 
+Resize:               + SHIFT + 
+Swap window:          + CTRL + 
+
+Create group:         + G
+Change group active:  + ALT + 
+
+Reload topbar:        + ALT + /
+_EOF_`
+
+notify-send -h string:bgcolor:#07273b "$msg1" -i none;
+notify-send -h string:bgcolor:#07273b "$msg2" -i none;
